487475f7d3d4c78c1944129402bcae4b241a6de3
Scott Stoltzman <scott@stoltzmaniac.com>
2017-08-03 22:00:02 -0600

    Getting Campaigns up to speed

--- Microsoft Vision API/sandbox.R
+++ Microsoft Vision API/sandbox.R
@@ -3,7 +3,7 @@ library(RCurl)
 library(httr)
 
 credentials = read.csv('credentials.csv')
-api_key <- as.character(credentials$subscription_id)
+api_key = as.character(credentials$subscription_id) #api key is not subscription id
 #imageURL <- "https://i1.wp.com/www.stoltzmaniac.com/wp-content/uploads/2017/07/unnamed-chunk-2-1.png?zoom=2&w=620&ssl=1"
 imageURL = 'https://i1.wp.com/www.stoltzmaniac.com/wp-content/uploads/2017/07/unnamed-chunk-4-1.png?zoom=2&w=620&ssl=1'
 endpointURL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze"
