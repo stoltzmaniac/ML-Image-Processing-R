38d53e1a930b3a4e0de0616f6c88f34aa9c63fe0
Scott Stoltzman <scott@stoltzmanconsulting.com>
2017-09-22 13:22:54 -0600

    Markdown file

--- Microsoft Vision API/sandbox.R
+++ Microsoft Vision API/sandbox.R
@@ -2,10 +2,10 @@ library(tidyverse)
 library(RCurl)
 library(httr)
 
-credentials = read.csv('credentials.csv')
+credentials = read_csv('credentials.csv')
 api_key = as.character(credentials$subscription_id) #api key is not subscription id
 #imageURL <- "https://i1.wp.com/www.stoltzmaniac.com/wp-content/uploads/2017/07/unnamed-chunk-2-1.png?zoom=2&w=620&ssl=1"
-imageURL = 'https://i1.wp.com/www.stoltzmaniac.com/wp-content/uploads/2017/07/unnamed-chunk-4-1.png?zoom=2&w=620&ssl=1'
+imageURL = 'http://www.stoltzmaniac.com/wp-content/uploads/2017/09/SnoozeGenius.jpg'
 endpointURL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze"
 
 
