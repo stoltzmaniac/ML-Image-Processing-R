a294daacf2a7877be0b7c707d89050e543f09bd0
Scott Stoltzman <scott@stoltzmanconsulting.com>
2017-09-22 22:25:15 -0600

    Added Microsoft signup pics

--- Microsoft Vision API/sandbox.R
+++ Microsoft Vision API/sandbox.R
@@ -1,21 +1,20 @@
 library(tidyverse)
 library(RCurl)
 library(httr)
+library(EBImage)
 
 credentials = read_csv('credentials.csv')
 api_key = as.character(credentials$subscription_id) #api key is not subscription id
-#imageURL <- "https://i1.wp.com/www.stoltzmaniac.com/wp-content/uploads/2017/07/unnamed-chunk-2-1.png?zoom=2&w=620&ssl=1"
-imageURL = 'http://www.stoltzmaniac.com/wp-content/uploads/2017/09/SnoozeGenius.jpg'
-endpointURL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze"
+api_endpoint_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze"
 
-
-visualFeatures = "Description,Tags,Categories"
-# options = Categories, Tags, Description, Faces, ImageType, Color, Adult
+image_url = 'https://imgur.com/rapIn0u.jpg'
+visualFeatures = "Description,Tags,Categories,Faces"
+# options = "Categories, Tags, Description, Faces, ImageType, Color, Adult"
 
 details = "Landmarks"
 # options = Landmarks, Celebrities
 
-reqURL = paste(endpointURL,
+reqURL = paste(api_endpoint_url,
                "?visualFeatures=",
                visualFeatures,
                "&details=",
@@ -25,8 +24,79 @@ reqURL = paste(endpointURL,
 APIresponse = POST(url = reqURL,
                    content_type('application/json'),
                    add_headers(.headers = c('Ocp-Apim-Subscription-Key' = api_key)),
-                   body=list(url = imageURL),
+                   body=list(url = image_url),
                    encode = "json") 
 
 df = content(APIresponse)
-df
+str(df)
+
+
+
+description_tags = df$description$tags
+description_tags_tib = tibble(tag = character())
+for(tag in description_tags){
+  for(text in tag){
+    if(class(tag) != "list"){  ## To remove the extra caption from being included
+      tmp = tibble(tag = tag)
+      description_tags_tib = description_tags_tib %>% bind_rows(tmp)
+    } 
+  }
+}
+
+knitr::kable(description_tags_tib[1:5,])
+
+
+captions = df$description$captions
+captions_tib = tibble(text = character(), confidence = numeric())
+for(caption in captions){
+  tmp = tibble(text = caption$text, confidence = caption$confidence)
+  captions_tib = captions_tib %>% bind_rows(tmp)
+}
+knitr::kable(captions_tib)
+
+
+metadata = df$metadata
+metadata_tib = tibble(width = metadata$width, height = metadata$height, format = metadata$format)
+knitr::kable(metadata_tib)
+
+
+
+faces = df$faces
+faces_tib = tibble(faceID = numeric(),
+                   age = numeric(), 
+                   gender = character(),
+                   x1 = numeric(),
+                   x2 = numeric(),
+                   y1 = numeric(),
+                   y2 = numeric())
+
+n = 0
+for(face in faces){
+  n = n + 1
+  tmp = tibble(faceID = n,
+               age = face$age, 
+               gender = face$gender,
+               x1 = face$faceRectangle$left,
+               y1 = face$faceRectangle$top,
+               x2 = face$faceRectangle$left + face$faceRectangle$width,
+               y2 = face$faceRectangle$top + face$faceRectangle$height)
+  faces_tib = faces_tib %>% bind_rows(tmp)
+}
+faces_tib
+knitr::kable(faces_tib)
+
+
+
+my_image <- readImage('SnoozeGenius.jpg')
+plot(my_image)
+
+coords = faces_tib %>% select(x1, y1, x2, y2)
+for(i in 1:nrow(coords)){
+  print(i)
+  xs = c(coords$x1[i], coords$x1[i], coords$x2[i], coords$x2[i])
+  ys = c(coords$y1[i], coords$y2[i], coords$y2[i], coords$y1[i])
+  polygon(x = xs, y = ys, border = i+1, lwd = 4)
+}
+
+
+
